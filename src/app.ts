bffc15789b78242296949efd230ff50ea63b262d
etychangucdavis <64785646+etychangucdavis@users.noreply.github.com>
2021-03-08 18:03:42 +0800

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+// Get environment variables
+import * as dotenv from 'dotenv'
+dotenv.config({ path: `${__dirname}/../.env` })
+
+// Dependencies
+import { Telegraf, ContextMessageUpdate } from 'telegraf'
+import { setupStartAndHelp } from './commands/startAndHelp'
+import { setupRandy } from './commands/randy'
+import { setupCallback, setupListener } from './helpers/raffle'
+import { setupLanguage, setupLanguageCallback } from './commands/language'
+import { setupNumberCallback, setupNumber } from './commands/number'
+import { setupTestLocale } from './commands/testLocales'
+import { setupSubscribe } from './commands/subscribe'
+import { setupNosubscribe } from './commands/nosubscribe'
+import { setupRaffleMessage } from './commands/raffleMessage'
+import { setupWinnerMessage } from './commands/winnerMessage'
+import { setupNodelete } from './commands/nodelete'
+import { setupListenForForwards } from './helpers/listenForForwards'
+import { setupConfigRaffle } from './commands/configRaffle'
+import { setupAddChat } from './commands/addChat'
+const telegraf = require('telegraf')
+
+// Setup the bot
+const bot: Telegraf<ContextMessageUpdate> = new telegraf(process.env.TOKEN, {
+  username: process.env.USERNAME,
+  channelMode: true,
+})
+bot.startPolling()
+console.log('Bot is up and running')
+
+// Setup callback
+setupCallback(bot)
+// Setup listeners
+setupListener(bot)
+setupListenForForwards(bot)
+
+// Setup commands
+setupStartAndHelp(bot)
+setupRandy(bot)
+setupLanguage(bot)
+setupNumber(bot)
+setupTestLocale(bot)
+setupSubscribe(bot)
+setupNosubscribe(bot)
+setupRaffleMessage(bot)
+setupWinnerMessage(bot)
+setupNodelete(bot)
+setupConfigRaffle(bot)
+setupAddChat(bot)
+
+// Setup callbacks
+setupLanguageCallback(bot)
+setupNumberCallback(bot)
+
+bot.catch(console.error)
